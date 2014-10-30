2494f90549e352870d39dd33ea3278cf3874a147
Andre Brutus <andre@brut.us>
2014-10-30 14:20:34 -0700

    add a default count of 0 to the stub for failed requests'

--- src/Osfi/Stub.php
+++ src/Osfi/Stub.php
@@ -4,12 +4,14 @@ namespace Osfi;
 class Stub {
     public $error;
     public $entities;
+    public $count;
     public $msg;
     public $status;
     public function __construct($error_msg) {
         $this->error = true;
         $this->msg = $error_msg;
         $this->entities = [];
+        $this->count = 0;
         $this->status = 500;
     }
 }
