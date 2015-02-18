61e8404ca387c8533d3e9cc791ef3aae2f3757e9
Andre Brutus <andre@brut.us>
2015-02-18 10:10:39 -0800

    add ofac exactname

--- src/Osfi/Client.php
+++ src/Osfi/Client.php
@@ -16,19 +16,19 @@ class Client {
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->ch, CURLOPT_TIMEOUT, self::TIMEOUT);
     }
-    public function matchName($name) {
-        return $this->__request(metaphone($name));
-    }
+
     public function matchExactName($name) {
         $transliterated = preg_replace("/[^A-Za-z0-9 ]/", '', transliterator_transliterate("Any-Latin; Latin-ASCII; Upper()", $name));
         $separated_by_lines = ltrim(join("-", array_filter(explode(" ", $transliterated))));
-        return $this->__request($separated_by_lines, "exactname/");
+        return $this->__request($separated_by_lines, "osfi/exactname/");
+    }
+    public function matchOfacExactName($name) {
+        $transliterated = preg_replace("/[^A-Za-z0-9 ]/", '', transliterator_transliterate("Any-Latin; Latin-ASCII; Upper()", $name));
+        $separated_by_lines = ltrim(join("-", array_filter(explode(" ", $transliterated))));
+        return $this->__request($separated_by_lines, "ofac/exactname/");
     }
 
-    public function matchMetaphone($name) {
-        return $this->__request($name);
-    }
-    private function __request($name, $route = "metaphone/") {
+    private function __request($name, $route = "exactname/") {
         curl_setopt($this->ch, CURLOPT_URL, $this->endpoint . $route . urlencode($name));
         $result = curl_exec($this->ch);
         if($result === FALSE) {
